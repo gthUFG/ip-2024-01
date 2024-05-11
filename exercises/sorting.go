669147cf4970f9810d92de3e653c1b7294104860
Gabriel Tessarolo Helmer <166325687+gthUFG@users.noreply.github.com>
2024-05-10 21:46:00 -0300

    Create sorting.go

---
+++ exercises/sorting.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// Insertion sorting
+
+func main() {
+	fmt.Print("Digite a lista: ")
+	l := inputToList()
+
+	insertionSorting := Method{
+		name: "Insertion Sorting",
+		sort: insertion,
+	}
+	sortMethods := []Method{insertionSorting}
+
+	fmt.Print("Digite o método a ser utilizado: ")
+	var x int
+	fmt.Scan(&x)
+	timer := time.NewTimer(time.Second)
+	sortMethods[x].sort(l)
+	timer.Stop()
+
+	fmt.Println(l)
+	fmt.Printf("O método %v foi utilizado, e foram gastos %v segundos para ordenar a lista.\n", sortMethods[x].name, timer)
+}
+
+type Method struct {
+	name string
+	sort func(l []float64) []float64
+}
+
+func insertion(l []float64) []float64 {
+	for i := 1; i < len(l)-1; i++ {
+		for j := i + 1; j > 0; j-- {
+			if l[j-1] < l[j] {
+				break
+			} else {
+				l[j], l[j-1] = l[j-1], l[j]
+			}
+		}
+	}
+	return l
+}
+
+func inputToList(params ...int) []float64 {
+	reader := bufio.NewReader(os.Stdin)
+	inp, _ := reader.ReadString('\n')
+	notes := toFloat64(strings.Split(strings.TrimSpace(inp), " "))
+	if len(params) != 0 {
+		if len(notes) != params[0] {
+			fmt.Println("\n\033[1;31mO input está incorreto.\n\033[m")
+			return []float64{}
+		}
+	}
+	return notes
+}
+func toFloat64(l1 []string) []float64 {
+	var floats = []float64{}
+	for i := range l1 {
+		l1[i] = strings.TrimSuffix(l1[i], "\n")
+		float, _ := strconv.ParseFloat(l1[i], 32)
+		floats = append(floats, float)
+	}
+	return floats
+}
